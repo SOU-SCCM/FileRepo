18f03443f50576186d2042091906a63bcad0b9d8
SOU-SCCM <63617227+SOU-SCCM@users.noreply.github.com>
2021-04-30 19:37:48 -0600

    Add files via upload

---
+++ DisableCoExistence.ps1
@@ -0,0 +1,25 @@
+﻿# Disable MDM coexistence and restore SCCM functionality
+# Warning: Airwatch stops sending certain telemetry while registry key is disabled
+# Backup key is saved to %PUBLIC% folder as "MDMbackup.reg"
+
+# James A. Chambers - https://jamesachambers.com
+
+$Enrollments = $null
+$AirwatchEnrollment = $null
+
+$Enrollments = Get-ChildItem -Path HKLM:\Software\Microsoft\Enrollments
+$AirwatchEnrollment = $Enrollments | Where-Object { $_.GetValue("ProviderID") -eq "AirWatchMDM" }
+
+if ($null -ne $AirwatchEnrollment) {
+    $AirwatchRegPath = $AirwatchEnrollment.Name
+
+    $AirwatchRegPath
+    $AirwatchEnrollment
+
+    reg export $AirwatchRegPath C:\Users\Public\MDMbackup.reg
+    reg delete $AirwatchRegPath /f
+
+    Restart-Service -Name CcmExec -Force
+}
+
+exit 0
